59f8166777e7398c0c1ba8b72e4d301e3fb8bd58
Alberto <bodyboarddinamic@gmail.com>
2020-01-29 15:52:47 +0000

    Registration screen first look

--- app/src/main/java/com/example/collabwithme/MainActivity.kt
+++ app/src/main/java/com/example/collabwithme/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.collabwithme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        registrationLinkTextView.setOnClickListener {
+            val intentGoToRegistrationActivity = Intent(this, RegistrationActivity::class.java)
+            startActivity(intentGoToRegistrationActivity)
+
+        }
+
     }
 }
